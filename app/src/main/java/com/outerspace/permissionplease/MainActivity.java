1f21757dd3fde86495814e1c19666733facaddb8
lsilva <lsilva@magicleap.com>
2018-09-09 17:17:35 -0400

    Practice is functional, ugly, but functional

--- app/src/main/java/com/outerspace/permissionplease/MainActivity.java
+++ app/src/main/java/com/outerspace/permissionplease/MainActivity.java
@@ -2,6 +2,7 @@ package com.outerspace.permissionplease;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraDevice;
@@ -9,11 +10,14 @@ import android.hardware.camera2.CameraManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int CAMERA_PERMISSION_REQUEST_CODE = 3;
     private Presenter presenter = new Presenter();
 
     @Override
@@ -23,7 +27,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickRequestPermission(View view) {
-        presenter.openCamera(this);
+        if(! presenter.openCamera(this)) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this, Manifest.permission.CAMERA)) {
+                new AlertDialog.Builder(this)
+                        .setMessage(R.string.retionale_permission)
+                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                requestCameraPermission();
+                            }
+                        })
+                        .create().show();
+            } else {
+                requestCameraPermission();
+            }
+        }
     }
 
+    private void requestCameraPermission() {
+        String[] arrayPermissions = new String[] {Manifest.permission.CAMERA};
+        ActivityCompat.requestPermissions(this,
+                arrayPermissions,
+                CAMERA_PERMISSION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            Log.d("LUIS", "onRequestPermission");
+            return;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
 }
--- app/src/main/java/com/outerspace/permissionplease/Presenter.java
+++ app/src/main/java/com/outerspace/permissionplease/Presenter.java
@@ -1,32 +1,49 @@
 package com.outerspace.permissionplease;
 
 import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+
+import static android.hardware.camera2.CameraMetadata.LENS_FACING_FRONT;
 
 public class Presenter {
 
-    public void openCamera(Context context) {
-        if(ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA)
+    public boolean openCamera(Activity activity) {
+        if(ContextCompat.checkSelfPermission(activity, Manifest.permission.CAMERA)
                 == PackageManager.PERMISSION_GRANTED) {
-            CameraManager manager = (CameraManager) context
-                    .getSystemService(Context.CAMERA_SERVICE);
+            CameraManager manager =
+                    (CameraManager) activity.getSystemService(Context.CAMERA_SERVICE);
             try {
                 String[] cameraIdList = manager.getCameraIdList();
+                for(String cameraId: cameraIdList) {
+                    CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraId);
+                    Integer lensFacing = characteristics.get(CameraCharacteristics.LENS_FACING);
+                    if(lensFacing.equals(LENS_FACING_FRONT)) {
+                        manager.openCamera(cameraId, getCallback(), null);
+                    }
+                }
                 if(cameraIdList.length > 0) {
                     String cameraId = cameraIdList[0];
                     manager.openCamera(cameraId, getCallback(), null);
                 }
-            } catch (CameraAccessException e) {
+            } catch (CameraAccessException | NullPointerException e) {
                 e.printStackTrace();
             }
+            return true;
         }
+        return false;
     }
+
 
     private CameraDevice.StateCallback getCallback() {
         return new CameraDevice.StateCallback() {
